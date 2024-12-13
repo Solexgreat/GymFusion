d8c0980209a1f85b4523ea6054d491b3eea939c8
Solexgreat <solomonGodkid@gmail.com>
2024-12-13 11:29:39 +0100

    udating intructors

--- src/instructors/instructors.controller.ts
+++ src/instructors/instructors.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { InstructorsService } from './instructors.service';
 import { CreateInstructorDto } from './dto/create-instructor.dto';
 import { UpdateInstructorDto } from './dto/update-instructor.dto';
@@ -7,28 +15,42 @@ import { UpdateInstructorDto } from './dto/update-instructor.dto';
 export class InstructorsController {
   constructor(private readonly instructorsService: InstructorsService) {}
 
-  @Post()
-  create(@Body() createInstructorDto: CreateInstructorDto) {
-    return this.instructorsService.create(createInstructorDto);
-  }
-
+  // Get all instructors
   @Get()
-  findAll() {
-    return this.instructorsService.findAll();
+  async getAllInstructors() {
+    return await this.instructorsService.getAllInstructors();
   }
 
+  // Get an instructor by ID
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.instructorsService.findOne(+id);
+  async getInstructorById(@Param('id') id: string) {
+    return await this.instructorsService.getInstructorById(id);
   }
 
+  // Get instructors by gym ID
+  @Get('gym/:gymId')
+  async getInstructorsByGym(@Param('gymId') gymId: string) {
+    return await this.instructorsService.getInstructorsByGym(gymId);
+  }
+
+  // Create an instructor directly
+  @Post()
+  async createInstructorDirectly(@Body() createInstructorDto: CreateInstructorDto) {
+    return await this.instructorsService.createInstructorDirectly(createInstructorDto);
+  }
+
+  // Update an instructor by ID
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateInstructorDto: UpdateInstructorDto) {
-    return this.instructorsService.update(+id, updateInstructorDto);
+  async updateInstructor(
+    @Param('id') id: string,
+    @Body() updateInstructorDto: UpdateInstructorDto,
+  ) {
+    return await this.instructorsService.updateInstructor(id, updateInstructorDto);
   }
 
+  // Delete an instructor by ID
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.instructorsService.remove(+id);
+  async deleteInstructor(@Param('id') id: string) {
+    return await this.instructorsService.deleteInstructor(id);
   }
 }
--- src/instructors/instructors.module.ts
+++ src/instructors/instructors.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { InstructorsService } from './instructors.service';
 import { InstructorsController } from './instructors.controller';
+import { Instructor } from './entities/instructor.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Gym } from 'src/gyms/entities/gym.entity';
+import { UserService } from 'src/user/user.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Instructor, User, Gym]),
+  ],
   controllers: [InstructorsController],
-  providers: [InstructorsService],
+  providers: [InstructorsService, UserService],
+  exports: [InstructorsService],
 })
 export class InstructorsModule {}
