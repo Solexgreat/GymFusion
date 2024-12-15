4b73a5cebabbd25de1960a5b86783e2a837333c2
Solexgreat <solomonGodkid@gmail.com>
2024-12-15 12:53:17 +0100

    jwt guards

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,28 +1,54 @@
-import { ExecutionContext, Injectable } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
-import { AuthGuard } from "@nestjs/passport";
-import { Observable } from "rxjs";
-import { IS_PUBLIC_KEY } from "../decorators/public.decorator";
-
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt'){
-    constructor(
-        private reflector: Reflector
-    ){
-        super()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(
+    private reflector: Reflector,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
     }
 
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-            context.getHandler(),
-            context.getClass()
-        ])
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization?.split(' ')[1];
 
-        if(isPublic){
-            return true
-        }
+    console.log(token);
 
-        return super.canActivate(context)
+    if (!token) {
+      throw new UnauthorizedException('Token missing');
     }
+
+    try {
+      const decoded = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get<string>('JWT_SECRET'),
+      });
+      request.user = decoded;
+      console.log(decoded);
+      return true;
+      // return super.canActivate(context)
+    } catch (error) {
+      console.error('error here', error);
+      throw new UnauthorizedException('Invalid or expired token');
+    }
+  }
 }
